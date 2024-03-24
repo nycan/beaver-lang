8c4c4f4985789ea48364393818ccce5d0ee30a33
nycan <ny.saskcan@gmail.com>
2024-03-24 09:29:44 -0600

    Finished basic parser

--- parser.hpp
+++ parser.hpp
@@ -108,9 +108,39 @@ public:
         }
     }
 
-    static std::unique_ptr<SyntaxTree> parseOpRHS(int t_minPrec,
-                                                  std::unique_ptr<SyntaxTree> t_LHS){
-        
+    static std::unique_ptr<SyntaxTree> parseOpRHS(
+        const int t_minPrec,
+        std::unique_ptr<SyntaxTree> t_leftSide
+    ){
+        while(true){
+            int precedence = BinopPrecedence();
+
+            //if it is lower precedence, then it will be parsed in a different call
+            if(precedence < t_minPrec){
+                return t_leftSide;
+            }
+
+            int operation = m_lexer.getChar();
+            m_lexer.nextToken();
+
+            auto rightSide = parseMain();
+            if(!rightSide){
+                return nullptr;
+            }
+
+            int nextPrec = BinopPrecedence();
+            if(precedence < nextPrec){
+                rightSide = parseOpRHS(precedence+1, std::move(rightSide));
+                if(!rightSide){
+                    return nullptr;
+                }
+            }
+
+            t_leftSide = std::make_unique<BinaryOpAST>(
+                operation,std::move(t_leftSide),
+                std::move(rightSide)
+            );
+        }
     }
 
     static std::unique_ptr<SyntaxTree> parseExpression(){
@@ -119,6 +149,48 @@ public:
             return nullptr;
         }
         return parseOpRHS(0, std::move(leftSide));
+    }
+
+    static std::unique_ptr<PrototypeAST> parsePrototype(){
+        if(m_lexer.getTok() != Token::IDENTIFIER){
+            std::cerr << "Expected function name in prototype.\n";
+            return nullptr;
+        }
+        
+        //function name
+        std::string funcName = m_lexer.getIdentifier();
+        m_lexer.nextToken();
+
+        //arguments
+        if(m_lexer.getChar() != '('){
+            std::cerr << "Expected '('\n";
+        }
+        std::vector<std::string> args;
+        while(m_lexer.nextToken() == Token::IDENTIFIER){
+            args.push_back(m_lexer.getIdentifier());
+        }
+        if(m_lexer.getChar() != ')'){
+            std::cerr << "Expected ')'\n";
+        }
+        m_lexer.nextToken();
+
+        return std::make_unique<PrototypeAST>(funcName,std::move(args));
+    }
+
+    static std::unique_ptr<FunctionAST> parseDefinition(){
+        //function declaration
+        m_lexer.nextToken();
+
+        //prototype
+        auto prototype = parsePrototype();
+        if(!prototype){
+            return nullptr;
+        }
+
+        if(auto expr = parseExpression()){
+            return std::make_unique<FunctionAST>(std::move(prototype),std::move(expr));
+        }
+        return nullptr;
     }
 };
 
