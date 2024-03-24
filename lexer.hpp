672fe9bd293d291d914d8beafb59c8910ccac887
nycan <ny.saskcan@gmail.com>
2024-03-23 22:16:59 -0600

    Made some modifications to the lexer, started work on parser

--- lexer.hpp
+++ lexer.hpp
@@ -4,70 +4,85 @@
 #include <string>
 #include <cctype>
 
+enum class Token{
+    UNKNOWN,
+    ENDFILE,
+    FUNC,
+    EXTERN,
+    IDENTIFIER,
+    NUMBER
+};
+
 class Lexer{
 private:
     static std::string m_identifier;
     static double m_numVal;
-    enum Token{
-        eofToken = -1,
-        funcToken = -2,
-        externToken = -3,
-        identifierToken = -4,
-        numberToken = -5
-    };
+    static Token m_currTok;
+    static int m_currChar;
+
 public:
-    static int nextToken(){
-        static int lastChar = ' ';
-        while(std::isspace(lastChar)){
-            lastChar = getchar();
+    inline std::string& getIdentifier() const {return m_identifier;}
+    inline double getNum() const {return m_numVal;}
+    inline Token getTok() const {return m_currTok;}
+    inline char getChar() const {return m_currChar;}
 
-            // Keywords and identifiers
-            if(std::isalpha(lastChar)){
-                m_identifier = lastChar;
-                while(std::isalnum(lastChar=getchar())){
-                    m_identifier += lastChar;
-                }
-
-                if(m_identifier=="fn"){
-                    return funcToken;
-                }
-                if(m_identifier=="extern"){
-                    return externToken;
-                }
-                return identifierToken;
-            }
-
-            // Numbers
-            if(std::isdigit(lastChar) || lastChar=='.'){
-                std::string numStr;
-                do{
-                    numStr += lastChar;
-                    lastChar = getchar();
-                } while (std::isdigit(lastChar) || lastChar=='.');
-
-                m_numVal = std::stoi(numStr);
-                return numberToken;
-            }
-
-            // Comments
-            if(lastChar=='#'){
-                do{
-                    lastChar = getchar();
-                } while (lastChar != EOF && lastChar != '\n' && lastChar != '\r');
-
-                if(lastChar != EOF){
-                    return nextToken();
-                }
-            }
-
-            if(lastChar==EOF){
-                return eofToken;
-            }
-
-            int currChar = lastChar;
-            lastChar = getchar();
-            return currChar;
+    //read the next token
+    static Token processToken(){
+        static int m_currChar = ' ';
+        while(std::isspace(m_currChar)){
+            m_currChar = getchar();
         }
+
+        // Keywords and identifiers
+        if(std::isalpha(m_currChar)){
+            m_identifier = m_currChar;
+            while(std::isalnum(m_currChar=getchar())){
+                m_identifier += m_currChar;
+            }
+
+            if(m_identifier=="fn"){
+                return Token::FUNC;
+            }
+            if(m_identifier=="extern"){
+                return Token::EXTERN;
+            }
+            return Token::IDENTIFIER;
+        }
+
+        // Numbers
+        if(std::isdigit(m_currChar) || m_currChar=='.'){
+            std::string numStr;
+            do{
+                numStr += m_currChar;
+                m_currChar = getchar();
+            } while (std::isdigit(m_currChar) || m_currChar=='.');
+
+            m_numVal = std::stoi(numStr);
+            return Token::NUMBER;
+        }
+
+        // Comments
+        if(m_currChar=='#'){
+            do{
+                m_currChar = getchar();
+            } while (m_currChar != EOF && m_currChar != '\n' && m_currChar != '\r');
+
+            if(m_currChar != EOF){
+                return nextToken();
+            }
+        }
+
+        if(m_currChar==EOF){
+            return Token::ENDFILE;
+        }
+
+        m_currChar = getchar();
+        return Token::UNKNOWN;
+    }
+
+    //almost the same as nextToken(), but also update m_currTok.
+    static Token nextToken(){
+        return m_currTok = processToken();
     }
 };
 
---
+++ parser.hpp
@@ -0,0 +1,125 @@
+#ifndef TESTLANG_PARSER__HPP
+#define TESTLANG_PARSER_HPP
+
+#include "lexer.hpp"
+#include "syntaxtree.hpp"
+#include <iostream>
+#include <cctype>
+#include <map>
+
+class Parser{
+private:
+    static Lexer m_lexer;
+
+public:
+    static std::unique_ptr<SyntaxTree> parseNum(){
+        auto result = std::make_unique<NumberAST>(m_lexer.getNum());
+        m_lexer.nextToken();
+        return std::move(result);
+    }
+
+    static std::unique_ptr<SyntaxTree> parseExpression();
+
+    static std::unique_ptr<SyntaxTree> parseParens(){
+        m_lexer.nextToken();
+        auto exprResult = parseExpression();
+        if (!exprResult){
+            return nullptr;
+        }
+        if(m_lexer.getChar() != ')'){
+            std::cerr << "Missing ')'\n";
+            return nullptr;
+        }
+    }
+
+    static std::unique_ptr<SyntaxTree> parseIdentifier(){
+        std::string idName = m_lexer.getIdentifier();
+        m_lexer.nextToken();
+
+        //variable
+        if(m_lexer.getChar() != '('){
+            return std::make_unique<SyntaxTree>(idName);
+        }
+
+        //function call
+        m_lexer.nextToken();
+        std::vector<std::unique_ptr<SyntaxTree>> args;
+        if(m_lexer.getChar() != ')'){
+            while(true){
+                if(auto argument = parseExpression()){
+                    args.push_back(std::move(argument));
+                } else {
+                    return nullptr;
+                }
+                
+                if(m_lexer.getChar() == ')'){
+                    break;
+                }
+
+                if(m_lexer.getChar() != ','){
+                    std::cerr << "Expected ')' or ',' in argument list.";
+                    return nullptr;
+                }
+                m_lexer.nextToken();
+            }
+        }
+
+        m_lexer.nextToken();
+        return std::make_unique<CallAST>(idName, std::move(args));
+    }
+
+    static std::unique_ptr<SyntaxTree> handleUnknown(){
+        switch(m_lexer.getChar()){
+            case '(':
+                return parseParens();
+            default:
+                std::cerr << "Unknown token\n";
+                return nullptr;
+        }
+    }
+
+    static std::unique_ptr<SyntaxTree> parseMain(){
+        switch(m_lexer.getTok()){
+            case Token::IDENTIFIER:
+                return parseIdentifier();
+            case Token::NUMBER:
+                return parseNum();
+            default:
+                return handleUnknown();
+        }
+    }
+
+    static int BinopPrecedence(){ //TODO: replace this with something better
+        switch(m_lexer.getChar()){
+            case '*':
+                return 3;
+            case '/':
+                return 3;
+            case '+':
+                return 2;
+            case '-':
+                return 2;
+            case '<':
+                return 1;
+            case '>':
+                return 1;
+            default:
+                return -1;
+        }
+    }
+
+    static std::unique_ptr<SyntaxTree> parseOpRHS(int t_minPrec,
+                                                  std::unique_ptr<SyntaxTree> t_LHS){
+        
+    }
+
+    static std::unique_ptr<SyntaxTree> parseExpression(){
+        auto leftSide = parseMain();
+        if(!leftSide){
+            return nullptr;
+        }
+        return parseOpRHS(0, std::move(leftSide));
+    }
+};
+
+#endif
