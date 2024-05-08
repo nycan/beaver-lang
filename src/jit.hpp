a84d4c4f90127296cab051fb275bfdbc95dc5e95
NY <67083864+nycan@users.noreply.github.com>
2024-05-08 21:19:03 +0000

    clang-formatted stuff

--- src/jit.hpp
+++ src/jit.hpp
@@ -13,20 +13,26 @@
 #include "llvm/IR/LLVMContext.h"
 #include <memory>
 
-class TestlangJIT{
+class TestlangJIT {
 private:
-    llvm::orc::ExecutionSession m_execSession;
-    llvm::orc::RTDyldObjectLinkingLayer m_objLayer;
-    llvm::orc::IRCompileLayer m_compLayer;
+  llvm::orc::ExecutionSession m_execSession;
+  llvm::orc::RTDyldObjectLinkingLayer m_objLayer;
+  llvm::orc::IRCompileLayer m_compLayer;
 
-    llvm::DataLayout m_layout;
-    llvm::orc::MangleAndInterner m_mangler;
-    llvm::orc::ThreadSafeContext m_context;
+  llvm::DataLayout m_layout;
+  llvm::orc::MangleAndInterner m_mangler;
+  llvm::orc::ThreadSafeContext m_context;
+
 public:
-    TestlangJIT(llvm::orc::JITTargetMachineBuilder t_jtmb, llvm::DataLayout t_layout):
-        m_execSession(std::make_unique<llvm::orc::ExecutorProcessControl>()),
-        m_objLayer(m_execSession, [](){return std::make_unique<llvm::SectionMemoryManager>();}),
-        m_compLayer(m_execSession, m_objLayer, std::make_unique<llvm::orc::ConcurrentIRCompiler>(std::move(t_jtmb))),
+  TestlangJIT(llvm::orc::JITTargetMachineBuilder t_jtmb,
+              llvm::DataLayout t_layout)
+      : m_execSession(std::make_unique<llvm::orc::ExecutorProcessControl>()),
+        m_objLayer(
+            m_execSession,
+            []() { return std::make_unique<llvm::SectionMemoryManager>(); }),
+        m_compLayer(m_execSession, m_objLayer,
+                    std::make_unique<llvm::orc::ConcurrentIRCompiler>(
+                        std::move(t_jtmb))),
         m_layout(std::move(t_layout)), m_mangler(m_execSession, this->m_layout),
         m_context(std::make_unique<llvm::LLVMContext>()) {}
 };
--- src/parser.hpp
+++ src/parser.hpp
@@ -6,11 +6,7 @@
 #include <cctype>
 #include <iostream>
 
-enum class ParserStatus {
-  ok,
-  end,
-  error
-};
+enum class ParserStatus { ok, end, error };
 
 class Parser {
 private:
@@ -36,8 +32,8 @@ private:
 public:
   Parser(std::unique_ptr<Lexer> t_lexer, std::shared_ptr<Generator> t_genData)
       : m_lexer(std::move(t_lexer)), m_genData(t_genData) {
-        m_lexer->nextToken();
-      }
+    m_lexer->nextToken();
+  }
   ~Parser() = default;
 
   ParserStatus parseOuter();
